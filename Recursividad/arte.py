62153e282dd5c2ca1c3ab9060bf8b15856a89843
Anfercode <andesecci12@gmail.com>
2019-06-17 22:02:39 -0500

    Se añade el ejemplo del greedy Algorithm

--- Recursividad/arte.py
+++ Recursividad/arte.py
@@ -5,10 +5,10 @@ myTurtle = turtle.Turtle()
 myWin = turtle.Screen()
 
 def Draw(myTurtle, length):
-    if length > 0:
-        myTurtle.forward(length)
-        myTurtle.left(90)
-        Draw(myTurtle,length-10)
+        if length > 0:
+                myTurtle.forward(length)
+                myTurtle.left(90)
+                Draw(myTurtle,length-10)
 
 Draw(myTurtle,100)
 myWin.exitonclick()
